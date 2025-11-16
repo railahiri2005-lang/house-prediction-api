84055999906ac77d31b09b52dba903f0ee2933d7
railahiri2005-lang <rai.lahiri2005@gmail.com>
2025-11-16 23:09:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load your trained model
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return "🏡 House Prediction API is running!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    features = np.array([[data['area'], data['bedrooms'], data['bathrooms']]])
+    prediction = model.predict(features)
+    return jsonify({"Predicted Price": float(prediction[0])})
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
