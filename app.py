b86289d7c877cb3e3a7909cd1c83390c477fc702
railahiri2005-lang <rai.lahiri2005@gmail.com>
2025-11-17 19:06:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+import pickle
+import numpy as np
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+# Load model
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+
+    area = float(data['area'])
+    bedrooms = int(data['bedrooms'])
+    bathrooms = int(data['bathrooms'])
+
+    features = np.array([[area, bedrooms, bathrooms]])
+
+    prediction = model.predict(features)[0]
+
+    if prediction < 0:
+        prediction = 0
+
+    return jsonify({"Predicted Price": round(prediction, 2)})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
